1b35eec11153cf1e56639e75bb4fef645e2163a4
sanober15 <sanober@sanober.me>
2017-12-03 21:44:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.
 //var theFunk = funkyFunction
 //funkyFunction();
-var theFunk = funkyFunction(){
+var theFunk = funkyFunction{
   return function(){
     return funkyFunction;
   }
