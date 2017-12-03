3ea86ff5f63772d1ab66a4fc2b69b724e8983655
sanober15 <sanober@sanober.me>
2017-12-03 22:53:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ var funkyFunction = function() {
 var theFunk = function(){
   var funkyFunction = "FUNKY!"
   return function funkyFunction(){
-    return console.log(funkyFunction);
+    return console.log(`${funkyFunction}`);
   }
 }
 
