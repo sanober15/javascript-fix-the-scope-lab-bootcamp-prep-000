41e9556ced9191d3b082b9176f882db7b1c08bd8
sanober15 <sanober@sanober.me>
2017-12-03 20:39:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,10 +14,13 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  return n + two
+
 
   // Feel free to move things around!
   const two = 2
+
+
+    return n + two;
 }
 
 var funkyFunction = function() {
