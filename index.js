05d9fe1aaa5be0ef144d2e2cb7d6d9d8cb257cf0
sanober15 <sanober@sanober.me>
2017-12-03 22:33:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var animal = 'dog'
 
 function myAnimal() {
-  return animal;
+  return animal
 }
 
 function yourAnimal() {
@@ -17,13 +17,13 @@ function add2(n) {
 
 
   // Feel free to move things around!
-  const two = 2;
+  const two = 2
 
 
     return n + two;
 }
 
-function funkyFunction() {
+var funkyFunction = function() {
   return function() {
     return "FUNKY!"
   }
@@ -31,12 +31,19 @@ function funkyFunction() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-funkyFunction;
-var theFunk = funkyFunction();
+var theFunk = function(){
+  var funkyFunction;
+  return function funkyFunction(){
+    return funkyFunction;
+  }
 
+}
+
+/*
 function theFunk(){
   var theFunk = "FUNKY!";
   return function funkyFunction(){
     return theFunk;
   }
 }
+*/
